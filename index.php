e21c15d4b73829e4e2c61b93bf1ded6fcd3da365
corriol <vjorda@gmail.com>
2021-11-01 19:46:38 +0100

    Creació de la base de dades i modificació d'index.php i movie.php

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
+declare(strict_types=1);
 
 require "src/Movie.php";
-require "movies.inc.php";
+// ara obtindrem les pel·lícules de la BD
+// require "movies.inc.php";
+
+$pdo = new PDO("mysql:host=mysql-server;dbname=movieFX;charset=utf8", "dbuser", "1234");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$moviesStmt = $pdo->prepare("SELECT * FROM movie");
+$moviesStmt->setFetchMode(PDO::FETCH_ASSOC);
+$moviesStmt->execute();
+
+// fetchAll tornarà un array les dades de pel·lícules en un altre array
+// caldrà mapejar les dades
+$moviesAr = $moviesStmt->fetchAll();
+
+foreach ($moviesAr as $movieAr) {
+    $movie = new Movie();
+    $movie->setId((int)$movieAr["id"]);
+    $movie->setTitle($movieAr["title"]);
+    $movie->setPoster($movieAr["poster"]);
+    $movie->setReleaseDate($movieAr["release-date"]);
+    $movie->setOverview($movieAr["overview"]);
+    $movie->setRating((float)$movieAr["rating"]);
+    $movies[] = $movie;
+}
 
 require "views/index.view.php";
--- movie.php
+++ movie.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 require "src/Movie.php";
 
 //creem l'array d'objectes Movie
-require "movies.inc.php";
+//require "movies.inc.php";
 
 // inicialitze les variables que necessitaré.
 $id = 0;
@@ -16,6 +16,30 @@ $idTemp = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 if (!empty($idTemp))
     $id = $idTemp;
 
+$pdo = new PDO("mysql:host=mysql-server;dbname=movieFX;charset=utf8", "dbuser", "1234");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$moviesStmt = $pdo->prepare("SELECT * FROM movie WHERE id=:id");
+$moviesStmt->bindValue("id", $id);
+$moviesStmt->setFetchMode(PDO::FETCH_ASSOC);
+$moviesStmt->execute();
+
+$movieAr = $moviesStmt->fetch();
+
+if (!empty($movieAr)) {
+    $movie = new Movie();
+    $movie->setId((int)$movieAr["id"]);
+    $movie->setTitle($movieAr["title"]);
+    $movie->setPoster($movieAr["poster"]);
+    $movie->setReleaseDate($movieAr["release-date"]);
+    $movie->setOverview($movieAr["overview"]);
+    $movie->setRating((float)$movieAr["rating"]);
+}
+else
+    $errors[] = "La pel·lícula sol·licitada no existeix";
+
+/*
+Tot aquest codi ja no és necessari
 $filteredMovies = array_filter($movies, function ($movie) use ($id) {
     if ($movie->getId()===$id)
         return true;
@@ -26,5 +50,5 @@ if (count($filteredMovies) === 1)
     $movie = array_shift($filteredMovies);
 else
     $errors[] = "La pel·lícula sol·licitada no existeix";
-
+*/
 require "views/movie.view.php";
