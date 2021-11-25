6f4edcbbef36996696ef8497cc01fc56701f1ceb
corriol <vjorda@gmail.com>
2021-11-25 20:09:02 +0100

    602: classe UploadedFileHandler.php segons passos

--- src/UploadedFileHandler.php
+++ src/UploadedFileHandler.php
@@ -1,17 +1,31 @@
 <?php
 class UploadedFileHandler {
     private array $uploadedFile;
+    private int $maxSize;
+    private array $acceptedTypes;
+    private string $type;
 
-    public function __construct() {
+    public function __construct(string $inputName, array $acceptedTypes = [], int $maxSize = 0) {
+       if (empty($_FILES[$inputName]))
+           throw new Exception("La clau no existeix");
+
+       $this->uploadedFile = $_FILES[$inputName];
+       if ($this->uploadedFile["error"] !== UPLOAD_ERR_OK )
+           throw new Exception("Error en la pujada");
 
 
+       if (empty($acceptedTypes) || in_array($this->uploadedFile, $acceptedTypes))
+            $this->acceptedTypes = $acceptedTypes;
+       else
+           throw new InvalidTypeFileException("El tipus del fitxer no és correcte");
+
+        if ($this->uploadedFile["size"] > $maxSize)
+           throw new TooBigFileException("El fitxer ({$this->uploadedFile["size"]}) és major que $maxSize");
+
+       $this->maxSize = $maxSize;
     }
     
     public function handle() {
-        if (!empty($_FILES['poster']) && ($_FILES['poster']['error'] == UPLOAD_ERR_OK)) {
-        if (!file_exists(Movie::POSTER_PATH))
-            mkdir(Movie::POSTER_PATH, 0777, true);
-
         $tempFilename = $_FILES["poster"]["tmp_name"];
         $currentFilename = $_FILES["poster"]["name"];
 
@@ -20,23 +34,10 @@ class UploadedFileHandler {
         $extension = explode("/", getFileExtension($tempFilename))[1];
         $newFilename = md5((string)rand()) . "." . $extension;
         $newFullFilename = Movie::POSTER_PATH . "/" . $newFilename;
-        $fileSize = $_FILES["poster"]["size"];
-
-        if (!in_array($mimeType, $validTypes))
-            throw new InvalidTypeFileException("La foto no és jpg");
-
-        if ($extension != 'jpeg')
-            throw new InvalidTypeFileException("La foto no és jpg");
-
-        if ($fileSize > MAX_SIZE)
-            throw new TooBigFileException("La foto té $fileSize bytes");
 
         if (!move_uploaded_file($tempFilename, $newFullFilename))
             throw new FileUploadException("No s'ha pogut moure la foto");
 
         $data["poster"] = $newFilename;
 
-    } else
-        throw new NoUploadedFileException("Cal pujar una photo");
-    }
 }
